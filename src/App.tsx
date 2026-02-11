fd573dc2fe75652006a79b4fe77c6665f60df299
Mateus <matbessa12@gmail.com>
2026-02-11 19:21:55 -0400

    Add dummy login gate before loading app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import { FullLayout } from './components';
+import { DummyLoginPage } from './components/DummyLoginPage';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  if (!isAuthenticated) {
+    return <DummyLoginPage onLoginSuccess={() => setIsAuthenticated(true)} />;
+  }
+
   return <FullLayout />;
 }
 
